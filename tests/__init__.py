562b06eac98a2b5ef581a761726f475b92c1a8bc
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2022-06-01 16:32:23 +0200

    add pytest tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""Test package for the `debug_this` package.
+
+This is an empty package. Please check the `Good Integration Practices`_.
+
+.. _Good Integration Practices:
+   https://docs.pytest.org/en/latest/explanation/goodpractices.html
+"""
+from __future__ import annotations
