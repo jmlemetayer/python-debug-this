055428a4b60418ee1b1eb097e53d11a1767fb438
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2022-06-02 18:10:58 +0200

    debug_this: bump to 0.3.0

--- debug_this/__init__.py
+++ debug_this/__init__.py
@@ -9,7 +9,7 @@ from typing import Callable
 
 module_logger = logging.getLogger(__name__)
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 def fucking_function(*args_d: Any, **kwargs_d: Any) -> Any:
