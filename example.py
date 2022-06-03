e5955efc203c7203f0f12f010b9eb213dc148e84
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2022-06-03 09:52:30 +0200

    add an example file

---
+++ example.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import logging
+
+import debug_this
+
+logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+
+@debug_this.fucking_function(print_parent=True)
+def example_function() -> None:
+    logger.info("This is an example function")
+
+
+@debug_this.fucking_class(logger)
+class ExampleClass:
+    def __init__(self) -> None:
+        logger.info("This is an example class constructor")
+        ExampleClass.example_static_method(self)
+
+    def example_method(self) -> None:
+        logger.info("This is an example class method")
+        example_function()
+
+    @staticmethod
+    def example_static_method(cls: ExampleClass) -> None:
+        logger.info("This is an example class static method")
+        cls.example_method()
+
+
+if __name__ == "__main__":
+    ExampleClass()
